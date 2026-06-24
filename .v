03ec543cb3f3526095c2f38fe246f693aec7683d
vedika <vedikalakade@gmail.com>
2026-06-24 14:52:41 +0530

    Delete .v

--- .v
+++
@@ -1,4 +0,0 @@
-The Smart Water Level Controller is a digital design project developed during the C2S (Chip to Startup) Training Program. The system is designed to monitor water levels in a storage tank and automatically control the water pump based on predefined level conditions.
-
-The project demonstrates the practical application of Verilog HDL, RTL design concepts, digital logic implementation, and simulation-based verification used in modern FPGA and VLSI design workflows.
-
